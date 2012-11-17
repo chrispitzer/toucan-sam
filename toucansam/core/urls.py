1916b2725580b5e0eebe5ebecacfe464a7bd5888
Chris Pitzer <chris@lofiart.com>
2012-11-17 15:03:08 -0800

    song list works - links to song page

--- toucansam/core/urls.py
+++ toucansam/core/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
     url(r'^songs/?$', 'core.views.song_list', name='song_list'),
-    url(r'^songs/(?P<song_id>[0-9]+)?$', 'core.views.song', name='song'),
+    url(r'^songs/(?P<song_id>[0-9]+)?$', 'core.views.song',
+        name='song'),
 )
