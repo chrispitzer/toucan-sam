334530658eb94fe0dee8afe8445c12ae54ce902a
Chase Caster <sealgair@gmail.com>
2013-10-29 19:33:30 -0700

    support heroku

--- toucansam/toucansam/settings.py
+++ toucansam/toucansam/settings.py
@@ -1,5 +1,5 @@
 # Django settings for toucansam project.
-
+import dj_database_url
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,14 +11,7 @@ MANAGERS = ADMINS
 GRAPPELLI_ADMIN_TITLE = "Toucan Sam"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'toucansam',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost/haikubattle')
 }
 
 TOUCAN_COLORS = [
