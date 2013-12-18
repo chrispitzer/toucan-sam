e88eb34f629e81f208036c9e31274023fcbeef7e
Chase Caster <sealgair@gmail.com>
2013-12-17 20:55:45 -0800

    add django_extensions to installed apps

--- toucansam/toucansam/settings.py
+++ toucansam/toucansam/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'south',
     'toucansam',
     'core',
