94766efb0325eb952eb274ccdd8f9a8eecfbc4dd
Chase Caster <sealgair@gmail.com>
2013-12-21 13:44:57 -0800

    mobile theme for song list

--- toucansam/core/urls.py
+++ toucansam/core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
-from core.views import SetListView, CheatSheetView, SetListAjax, RandoColor, SetListSecondsjaxView, SongView
+from core.views import SetListView, CheatSheetView, SetListAjax, RandoColor, SetListSecondsjaxView, SongView, SongListView
 
 urlpatterns = patterns('',
-    url(r'^/?$', 'core.views.song_list', name='song_list'),
-    url(r'^songs/?$', 'core.views.song_list', name='song_list'),
+    url(r'^/?$', SongListView.as_view(), name='song_list'),
+    url(r'^songs/?$', SongListView.as_view(), name='song_list'),
     url(r'^set_list/(?P<set_list_id>\d+|new)/?$', SetListView.as_view(), name='set_list'),
     url(r'^songs/(?P<song_id>[0-9]+)?$', SongView.as_view(), name='song'),
     url(r'^set_list_list/', 'core.views.set_list_list', name='set_list_list'),
--- toucansam/core/views.py
+++ toucansam/core/views.py
@@ -1,4 +1,6 @@
 import json
+import os
+import re
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
@@ -11,18 +13,86 @@ from lib.decorators import template
 from django.shortcuts import get_object_or_404, redirect
 
 
-@template("home.html")
-def home(request):
-    print "HOME"
+class MobileTemplateView(TemplateView):
+    is_mobile = False
+
+    def is_request_mobile(self, request):
+        """
+        Snipped pulled from https://djangosnippets.org/snippets/2001/
+        """
+        if 'mobile' in request.GET:
+            return request.GET.get('mobile', 'no').lower() in ('yes', 'true', '1')
+
+        is_mobile = False
+
+        if request.META.has_key('HTTP_USER_AGENT'):
+            user_agent = request.META['HTTP_USER_AGENT']
+
+            # Test common mobile values.
+            pattern = "(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|windows ce|pda|mobile|mini|palm|netfront)"
+            prog = re.compile(pattern, re.IGNORECASE)
+            match = prog.search(user_agent)
+
+            if match:
+                is_mobile = True
+            else:
+                # Nokia like test for WAP browsers.
+                # http://www.developershome.com/wap/xhtmlmp/xhtml_mp_tutorial.asp?page=mimeTypesFileExtension
+
+                if request.META.has_key('HTTP_ACCEPT'):
+                    http_accept = request.META['HTTP_ACCEPT']
+
+                    pattern = "application/vnd\.wap\.xhtml\+xml"
+                    prog = re.compile(pattern, re.IGNORECASE)
+
+                    match = prog.search(http_accept)
+
+                    if match:
+                        is_mobile = True
+
+            if not is_mobile:
+                # Now we test the user_agent from a big list.
+                user_agents_test = ("w3c ", "acs-", "alav", "alca", "amoi", "audi",
+                                    "avan", "benq", "bird", "blac", "blaz", "brew",
+                                    "cell", "cldc", "cmd-", "dang", "doco", "eric",
+                                    "hipt", "inno", "ipaq", "java", "jigs", "kddi",
+                                    "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-",
+                                    "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                                    "mot-", "moto", "mwbp", "nec-", "newt", "noki",
+                                    "xda",  "palm", "pana", "pant", "phil", "play",
+                                    "port", "prox", "qwap", "sage", "sams", "sany",
+                                    "sch-", "sec-", "send", "seri", "sgh-", "shar",
+                                    "sie-", "siem", "smal", "smar", "sony", "sph-",
+                                    "symb", "t-mo", "teli", "tim-", "tosh", "tsm-",
+                                    "upg1", "upsi", "vk-v", "voda", "wap-", "wapa",
+                                    "wapi", "wapp", "wapr", "webc", "winw", "winw",
+                                    "xda-",)
+
+                test = user_agent[0:4].lower()
+                if test in user_agents_test:
+                    is_mobile = True
+
+        return is_mobile
+
+    def dispatch(self, request, *args, **kwargs):
+        self.is_mobile = self.is_request_mobile(request)
+        return super(MobileTemplateView, self).dispatch(request, *args, **kwargs)
+
+    def get_template_names(self):
+        templates = super(MobileTemplateView, self).get_template_names()
+        if self.is_mobile:
+            templates = [os.path.join('mobile', t) for t in templates]
+        return templates
 
 
-@template("song_list.html")
-def song_list(request):
-    songs = Song.active_objects.all()
-    return {
-        "songs": songs,
-        "set_list_id": "new",
-    }
+class SongListView(MobileTemplateView):
+    template_name = "song_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SongListView, self).get_context_data(**kwargs)
+        context['songs'] = Song.active_objects.all()
+        context['set_list_id'] = "new"
+        return context
 
 
 class SetListView(TemplateView):
